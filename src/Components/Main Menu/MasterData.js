f443be7f405064a8bb9f97f61230242c6a2c72f1
bryantobing12 <bryantobing0@gmail.com>
2021-07-18 00:16:55 +0700

    Draft: Update login state

--- src/Components/Main Menu/MasterData.js
+++ src/Components/Main Menu/MasterData.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { Box, Container, Flex } from "@chakra-ui/react";
+import { Box, Container, Flex, Button } from "@chakra-ui/react";
+import { Redirect, useHistory } from "react-router-dom";
+
 import TopBar from "../Navigation/TopBar";
 import Navbar from "../Navigation/Navbar";
 import Shorcut from "../Shorcut";
@@ -7,9 +9,24 @@ import Chart from "../Chart";
 import Reports from "../Reports";
 
 function MasterData() {
+  const history = useHistory();
+
+  if (!localStorage.getItem("authToken")) {
+    return <Redirect to="/login" />;
+  }
+
   return (
     <>
       <TopBar />
+
+      <Button
+        onClick={() => {
+          localStorage.removeItem("authToken");
+          history.push("/login");
+        }}
+      >
+        Logout
+      </Button>
 
       <Box bg="white" w="full" mt={4}>
         <Container maxW="container.xl" pt={2}>
